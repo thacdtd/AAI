09b17482e9a806b926d1799f22c2074b726672ff
Thac <thacdtd@gmail.com>
2017-06-27 16:26:54 +1000

    JML

---
+++ JML/sample.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+g = np.array([[1, 1, 1, 0, 0, 0, 0, 0, 0],
+              [1, 1, 1, 0, 0, 0, 0, 0, 0],
+              [1, 1, 1, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 1, 1, 0, 0, 0, 0],
+              [0, 0, 0, 1, 1, 0, 1, 0, 0],
+              [0, 0, 0, 0, 0, 1, 1, 1, 1],
+              [0, 0, 0, 0, 1, 1, 1, 1, 1],
+              [0, 0, 0, 0, 0, 1, 1, 1, 0],
+              [0, 0, 0, 0, 0, 1, 1, 0, 1]])
+
+fig, ax = plt.subplots(1,1)
+
+ax.imshow(g, cmap='Greys',  interpolation='nearest')
+x = ['1', '4', '6', '5', '8' ,'2', '3', '7', '9']
+plt.xticks(range(len(x)), x, fontsize=30)
+
+y = ['1', '4', '6', '5', '8' ,'2', '3', '7', '9']
+plt.yticks(range(len(y)), y, fontsize=30)
+
+
+plt.show()

70b0a68f96cfc322dc1836e41fef8be48fec5d96
Thac <thacdtd@gmail.com>
2017-12-14 12:38:51 +1100

    CPF

--- IJCAI2018/pf.py
+++ IJCAI2018/pf.py
@@ -14,11 +14,11 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class PoissonMF(BaseEstimator, TransformerMixin):
-    ''' Poisson matrix factorization with batch inference '''
-    def __init__(self, n_components=100, max_iter=100, tol=0.0005,
-                 smoothness=100, random_state=None, verbose=False,
+    """" Poisson matrix factorization with batch inference """
+    def __init__(self, n_components=100, max_iter=100, tol=0.0005, smoothness=100, random_state=None, verbose=False,
                  **kwargs):
-        ''' Poisson matrix factorization
+        """"
+        Poisson matrix factorization
 
         Arguments
         ---------
@@ -43,7 +43,7 @@ class PoissonMF(BaseEstimator, TransformerMixin):
 
         **kwargs: dict
             Model hyperparameters
-        '''
+        """
 
         self.n_components = n_components
         self.max_iter = max_iter
@@ -65,16 +65,15 @@ class PoissonMF(BaseEstimator, TransformerMixin):
 
     def _init_components(self, n_feats):
         # variational parameters for beta
-        self.gamma_b = self.smoothness \
-            * np.random.gamma(self.smoothness, 1. / self.smoothness,
-                              size=(self.n_components, n_feats))
-        self.rho_b = self.smoothness \
-            * np.random.gamma(self.smoothness, 1. / self.smoothness,
-                              size=(self.n_components, n_feats))
+        self.gamma_b = self.smoothness * np.random.gamma(self.smoothness, 1. / self.smoothness,
+                                                         size=(self.n_components, n_feats))
+        self.rho_b = self.smoothness * np.random.gamma(self.smoothness, 1. / self.smoothness,
+                                                       size=(self.n_components, n_feats))
         self.Eb, self.Elogb = _compute_expectations(self.gamma_b, self.rho_b)
 
     def set_components(self, shape, rate):
-        '''Set the latent components from variational parameters.
+        """
+        Set the latent components from variational parameters.
 
         Parameters
         ----------
@@ -88,7 +87,7 @@ class PoissonMF(BaseEstimator, TransformerMixin):
         -------
         self : object
             Return the instance itself.
-        '''
+        """
 
         self.gamma_b, self.rho_b = shape, rate
         self.Eb, self.Elogb = _compute_expectations(self.gamma_b, self.rho_b)
@@ -96,28 +95,21 @@ class PoissonMF(BaseEstimator, TransformerMixin):
 
     def _init_weights(self, n_samples):
         # variational parameters for theta
-        self.gamma_t = self.smoothness \
-            * np.random.gamma(self.smoothness, 1. / self.smoothness,
-                              size=(n_samples, self.n_components))
-        self.rho_t = self.smoothness \
-            * np.random.gamma(self.smoothness, 1. / self.smoothness,
-                              size=(n_samples, self.n_components))
+        self.gamma_t = self.smoothness * np.random.gamma(self.smoothness, 1. / self.smoothness,
+                                                         size=(n_samples, self.n_components))
+        self.rho_t = self.smoothness * np.random.gamma(self.smoothness, 1. / self.smoothness,
+                                                       size=(n_samples, self.n_components))
         self.Et, self.Elogt = _compute_expectations(self.gamma_t, self.rho_t)
         self.c = 1. / np.mean(self.Et)
 
     def fit(self, X):
-        '''Fit the model to the data in X.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_feats)
+        """
+        Fit the model to the data in X.
+        :param X: array-like, shape (n_samples, n_feats)
             Training data.
-
-        Returns
-        -------
-        self: object
+        :return: self: object
             Returns the instance itself.
-        '''
+        """
         n_samples, n_feats = X.shape
         self._init_components(n_feats)
         self._init_weights(n_samples)
@@ -125,28 +117,20 @@ class PoissonMF(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, attr=None):
-        '''Encode the data as a linear combination of the latent components.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_feats)
-
-        attr: string
+        """
+        Encode the data as a linear combination of the latent components.
+        :param X: array-like, shape (n_samples, n_feats)
+        :param attr: string
             The name of attribute, default 'Eb'. Can be changed to Elogb to
             obtain E_q[log beta] as transformed data.
-
-        Returns
-        -------
-        X_new : array-like, shape(n_samples, n_filters)
+        :return: X_new : array-like, shape(n_samples, n_filters)
             Transformed data, as specified by attr.
-        '''
-
+        """
         if not hasattr(self, 'Eb'):
             raise ValueError('There are no pre-trained components.')
         n_samples, n_feats = X.shape
         if n_feats != self.Eb.shape[1]:
-            raise ValueError('The dimension of the transformed data '
-                             'does not match with the existing components.')
+            raise ValueError('The dimension of the transformed data does not match with the existing components.')
         if attr is None:
             attr = 'Et'
         self._init_weights(n_samples)
@@ -163,10 +147,8 @@ class PoissonMF(BaseEstimator, TransformerMixin):
             bound = self._bound(X)
             improvement = (bound - old_bd) / abs(old_bd)
             if self.verbose:
-                sys.stdout.write('\r\tAfter ITERATION: %d\tObjective: %.2f\t'
-                                 'Old objective: %.2f\t'
-                                 'Improvement: %.5f' % (i, bound, old_bd,
-                                                        improvement))
+                sys.stdout.write('\r\tAfter ITERATION: %d\tObjective: %.2f\t Old objective: %.2f\t'
+                                 'Improvement: %.5f' % (i, bound, old_bd, improvement))
                 sys.stdout.flush()
             if improvement < self.tol:
                 break
@@ -177,23 +159,21 @@ class PoissonMF(BaseEstimator, TransformerMixin):
 
     def _update_theta(self, X):
         ratio = X / self._xexplog()
-        self.gamma_t = self.a + np.exp(self.Elogt) * np.dot(
-            ratio, np.exp(self.Elogb).T)
+        self.gamma_t = self.a + np.exp(self.Elogt) * np.dot(ratio, np.exp(self.Elogb).T)
         self.rho_t = self.a * self.c + np.sum(self.Eb, axis=1)
         self.Et, self.Elogt = _compute_expectations(self.gamma_t, self.rho_t)
         self.c = 1. / np.mean(self.Et)
 
     def _update_beta(self, X):
         ratio = X / self._xexplog()
-        self.gamma_b = self.b + np.exp(self.Elogb) * np.dot(
-            np.exp(self.Elogt).T, ratio)
+        self.gamma_b = self.b + np.exp(self.Elogb) * np.dot(np.exp(self.Elogt).T, ratio)
         self.rho_b = self.b + np.sum(self.Et, axis=0, keepdims=True).T
         self.Eb, self.Elogb = _compute_expectations(self.gamma_b, self.rho_b)
 
     def _xexplog(self):
-        '''
+        """
         sum_k exp(E[log theta_{ik} * beta_{kd}])
-        '''
+        """
         return np.dot(np.exp(self.Elogt), np.exp(self.Elogb))
 
     def _bound(self, X):
@@ -208,46 +188,34 @@ class PoissonMF(BaseEstimator, TransformerMixin):
 
 
 class OnlinePoissonMF(PoissonMF):
-    ''' Poisson matrix factorization with stochastic inference '''
+    # Poisson matrix factorization with stochastic inference
     def __init__(self, n_components=100, batch_size=10, n_pass=10,
                  max_iter=100, tol=0.0005, shuffle=True, smoothness=100,
                  random_state=None, verbose=False,
                  **kwargs):
-        ''' Poisson matrix factorization
-
-        Arguments
-        ---------
-        n_components : int
+        """
+        Poisson matrix factorization
+        :param n_components: int
             Number of latent components
-
-        batch_size : int
+        :param batch_size: int
             The size of mini-batch
-
-        n_pass : int
+        :param n_pass: int
             The number of passes through the entire data
-
-        max_iter : int
+        :param max_iter: int
             Maximal number of iterations to perform for a single mini-batch
-
-        tol : float
-            The threshold on the increase of the objective to stop the
-            iteration
-
-        shuffle : bool
+        :param tol: float
+            The threshold on the increase of the objective to stop theiteration
+        :param shuffle: bool
             Whether to shuffle the data or not
-
-        smoothness : int
+        :param smoothness: int
             Smoothness on the initialization variational parameters
-
-        random_state : int or RandomState
+        :param random_state: int or RandomState
             Pseudo random number generator used for sampling
-
-        verbose : bool
+        :param verbose: bool
             Whether to show progress during model fitting
-
-        **kwargs: dict
+        :param kwargs: dict
             Model hyperparameters and learning rate
-        '''
+        """
 
         self.n_components = n_components
         self.batch_size = batch_size
@@ -273,22 +241,15 @@ class OnlinePoissonMF(PoissonMF):
         self.kappa = float(kwargs.get('kappa', 0.6))
 
     def fit(self, X, est_total=None):
-        '''Fit the model to the data in X. X has to be loaded into memory.
-
-        Parameters
-        ----------
-        X : array-like, shape (n_samples, n_feats)
+        """
+        Fit the model to the data in X. X has to be loaded into memory.
+        :param X: array-like, shape (n_samples, n_feats)
             Training data.
-
-        est_total : int
-            The estimated size of the entire data. Could be larger than the
-            actual size.
-
-        Returns
-        -------
-        self: object
+        :param est_total: int
+            The estimated size of the entire data. Could be larger than the actual size.
+        :return: self: object
             Returns the instance itself.
-        '''
+        """
         n_samples, n_feats = X.shape
         if est_total is None:
             self._scale = float(n_samples) / self.batch_size
@@ -303,8 +264,7 @@ class OnlinePoissonMF(PoissonMF):
             if self.shuffle:
                 np.random.shuffle(indices)
             X_shuffled = X[indices]
-            for (i, istart) in enumerate(xrange(0, n_samples,
-                                                self.batch_size), 1):
+            for (i, istart) in enumerate(xrange(0, n_samples, self.batch_size), 1):
                 print '\tMinibatch %d:' % i
                 iend = min(istart + self.batch_size, n_samples)
                 self.set_learning_rate(iter=i)
@@ -314,48 +274,34 @@ class OnlinePoissonMF(PoissonMF):
         return self
 
     def partial_fit(self, X):
-        '''Fit the data in X as a mini-batch and update the parameter by taking
-        a natural gradient step. Could be invoked from a high-level out-of-core
-        wrapper.
-
-        Parameters
-        ----------
-        X : array-like, shape (batch_size, n_feats)
+        """
+        Fit the data in X as a mini-batch and update the parameter by taking a natural gradient step. Could be invoked
+        from a high-level out-of-core wrapper.
+        :param X: rray-like, shape (batch_size, n_feats)
             Mini-batch data.
-
-        Returns
-        -------
-        self: object
+        :return: self: object
             Returns the instance itself.
-        '''
+        """
         self.transform(X)
         # take a (natural) gradient step
         ratio = X / self._xexplog()
-        self.gamma_b = (1 - self.rho) * self.gamma_b + self.rho * \
-            (self.b + self._scale * np.exp(self.Elogb) *
-             np.dot(np.exp(self.Elogt).T, ratio))
-        self.rho_b = (1 - self.rho) * self.rho_b + self.rho * \
-            (self.b + self._scale * np.sum(self.Et, axis=0, keepdims=True).T)
+        self.gamma_b = (1 - self.rho) * self.gamma_b + self.rho * (self.b + self._scale * np.exp(self.Elogb) *
+                                                                   np.dot(np.exp(self.Elogt).T, ratio))
+        self.rho_b = (1 - self.rho) * self.rho_b + self.rho * (self.b + self._scale *
+                                                               np.sum(self.Et, axis=0, keepdims=True).T)
         self.Eb, self.Elogb = _compute_expectations(self.gamma_b, self.rho_b)
         return self
 
     def set_learning_rate(self, iter=None, rho=None):
-        '''Set the learning rate for the gradient step
-
-        Parameters
-        ----------
-        iter : int
-            The current iteration, used to compute a Robbins-Monro type
-            learning rate
-        rho : float
-            Directly specify the learning rate. Will override the one computed
-            from the current iteration.
-
-        Returns
-        -------
-        self: object
+        """
+        Set the learning rate for the gradient step
+        :param iter: int
+            The current iteration, used to compute a Robbins-Monro type learning rate
+        :param rho: float
+            Directly specify the learning rate. Will override the one computed from the current iteration.
+        :return: self: object
             Returns the instance itself.
-        '''
+        """
         if rho is not None:
             self.rho = rho
         elif iter is not None:
@@ -366,22 +312,19 @@ class OnlinePoissonMF(PoissonMF):
 
     def _stoch_bound(self, X):
         bound = np.sum(X * np.log(self._xexplog()) - self.Et.dot(self.Eb))
-        bound += _gamma_term(self.a, self.a * self.c, self.gamma_t, self.rho_t,
-                             self.Et, self.Elogt)
+        bound += _gamma_term(self.a, self.a * self.c, self.gamma_t, self.rho_t, self.Et, self.Elogt)
         bound += self.n_components * X.shape[0] * self.a * np.log(self.c)
         bound *= self._scale
-        bound += _gamma_term(self.b, self.b, self.gamma_b, self.rho_b,
-                             self.Eb, self.Elogb)
+        bound += _gamma_term(self.b, self.b, self.gamma_b, self.rho_b, self.Eb, self.Elogb)
         return bound
 
 
 def _compute_expectations(alpha, beta):
-    '''
+    """
     Given x ~ Gam(alpha, beta), compute E[x] and E[log x]
-    '''
-    return (alpha / beta, special.psi(alpha) - np.log(beta))
+    """
+    return alpha / beta, special.psi(alpha) - np.log(beta)
 
 
 def _gamma_term(a, b, shape, rate, Ex, Elogx):
-    return np.sum((a - shape) * Elogx - (b - rate) * Ex +
-                  (special.gammaln(shape) - shape * np.log(rate)))
+    return np.sum((a - shape) * Elogx - (b - rate) * Ex + (special.gammaln(shape) - shape * np.log(rate)))
